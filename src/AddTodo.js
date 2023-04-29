54741f272f1cd5afe6272b6455f8d1783d312e95
AnupShrestha28 <anupstha0168@gmail.com>
2023-04-30 00:50:15 +0545

    Done

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,41 +1,55 @@
-import React from "react";
+import React, { Component } from 'react'
 
-class Todo extends React.Component{
+export default class App extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            list : [],
-            addTodo : ""
-        };
+  constructor(){
+    super();
+    this.state = {
+      list : [],
+      addTodo : ""
+    }
+  }
+
+  componentDidMount(){
+    const storedData = localStorage.getItem("todoList");
+    if(storedData){
+      this.setState({list : JSON.parse(storedData)})
+    }
+  }
+
+  componentDidUpdate(prevState){
+    if(prevState.list !== this.state.list){
+      localStorage.setItem("todoList", JSON.stringify(this.state.list))
     }
 
-    inputText = (e) => {
-        this.setState({addTodo:e.target.value});
-    }
+  }
 
-    buttonAddTodo = (e) =>{
-        e.preventDefault();
-    }
+  inputText = (e) => {
+    this.setState({addTodo : e.target.value});
+  }
 
-    render(){
-        const {list,addTodo} = this.state;
-        return(
-            <>
-                <h1>{this.props.title}</h1>
-                <form className="form-info" onSubmit={this.buttonAddTodo}>
-                    <input type="text" placeholder="Enter your todo" value={addTodo} onChange={this.inputText}/>
-                    <button className="btn-info" onClick={() => {
-                        const{list, addTodo} = this.state;
-                        this.setState({list:[...list,addTodo], addTodo:""})
-                    }}>Add</button>
-                </form>
-                <ul>
-                    {list.map((lists, index) => <li className="list-info" key={index}>{lists}</li>)}
-                </ul>
-            </>
-        );
-    }
+  buttonAddTodo = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    const{list, addTodo} = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.buttonAddTodo}>
+          <input type='text' placeholder='Enter your todo' value={addTodo} onChange={this.inputText} />
+          <button onClick={() => {
+            const {list, addTodo} = this.state;
+            this.setState({list : [...list, addTodo], addTodo : ""})
+          }}>Add</button>
+        </form>
+
+          <ul>
+            {list?.map((lists, index) => <li key={index}>{lists}</li>)}
+          </ul>
+
+      </div>
+    )
+  }
 }
-
-export default Todo;
